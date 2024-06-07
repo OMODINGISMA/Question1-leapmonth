d868265d7935d74c0004d75869a5d057f15d50d9
OMODINGISMA <omodingismael@gmail.com>
2024-06-07 13:06:18 +0300

    Add files via upload

---
+++ leapmonth.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class LeapMonth {
+    public static void main(String[] args) {
+        int month;
+        Scanner mm = new Scanner(System.in);
+        System.out.println("This program examines the  year to confirm if its  a leap month or not");
+
+        System.out.println("please enter the month");
+
+    month=mm.nextInt();
+    int result=month%4;
+    if (result==0){
+        System.out.println(
+                "The year"+ month+ " is a leap month"
+        );
+    }else{
+        System.out.println(
+                "The year"+ month+ " is not a leap year"
+        );
+    }
+
+    }
+}
